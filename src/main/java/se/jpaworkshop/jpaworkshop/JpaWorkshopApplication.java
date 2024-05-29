b32d12c3b8e580d9a11e06bbbedf99682b58c3ae
ropa99 <rpahlin@hotmail.com>
2024-05-29 08:53:09 +0200

    Initial commit

---
+++ src/main/java/se/jpaworkshop/jpaworkshop/JpaWorkshopApplication.java
@@ -0,0 +1,13 @@
+package se.jpaworkshop.jpaworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWorkshopApplication.class, args);
+	}
+
+}
